bd2840b35ae9d746999a75af8558e3aede46107e
LeshiyAda9H <lesh_da9h@bk.ru>
2022-10-11 11:50:04 +0300

    add task3

---
+++ task3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    
+	cout<<"input n"<<endl;
+	cin>>n;
+	
+	for(int i=1; i<=n ; ++i)
+	{
+	    for (int k=n; i<=k; --k){
+	        cout<<' ';
+	    }
+	    for (int j=1; j<=i; ++j)
+	    {
+	        
+	        if (j!=1){
+	            cout<<'*';
+	        }
+	        cout<<'*';
+	    }
+	    cout<<endl;
+	}
+	
+	//////////
+	
+	for(int i=1; i<=n ; ++i)
+	{
+	    for (int j=1; j<=i; ++j)
+	    {
+	        cout<<' ';
+	    }
+	    cout<<' ';
+	    for (int k=n-1; i<=k; --k)
+	    {
+	        if (k!=4)
+	        {
+	        cout<<'*';
+	        }
+	        cout<<'*';
+	        
+	    }
+	    cout<<endl;
+	}
+	
+    return 0;
+}
